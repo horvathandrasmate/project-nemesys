ff0e730a2912f1ba6142d7359be344577007b1ce
Gyula Kiri <duszmo@f.fazekas.hu>
2020-04-04 14:23:53 +0200

    x scrolling patinent

--- application/views/patient/index.php
+++ application/views/patient/index.php
@@ -1,5 +1,5 @@
     <?php
-    
+
     ?>
     <link rel="stylesheet" href="<?php echo css_url("patient/style.css") ?>">
     <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
@@ -11,60 +11,63 @@
                 "paging": true,
                 "autoWidth": true,
                 "buttons": [
-                    
+
                 ],
                 "bPaginate": false,
-    
-    "bFilter": true,
-    "bInfo": false,
-    "bAutoWidth": false
+                "sScrollX": "110%",
+                "sScrollY": "200px",
+                "bScrollCollapse": true,
+                
+                "bFilter": true,
+                "bInfo": false,
+                // "bAutoWidth": false
             });
             $('.dataTables_filter').addClass('filter-css');
         });
     </script>
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <script>
+        <script>
 
-    </script>
-    <div class="container main-container" >
-        <h1 class="main-h1">Páciensek</h1>
-        <hr class="main-hr">
-        <div class="main-table">
-            <table id="table-container" style="max-width:1000px !important;overflow-x:auto !important;" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                <thead>
-                    <tr>
-                        <!--lang-->
-                        <th style='white-space:nowarp'>ID</th>
-                        <th style='white-space:nowarp'>NÉV</th>
-                        <th style='white-space:nowarp'>TAJSZÁM</th>
-                        <th style='white-space:nowarp'>SZÜLETÉSI DÁTUM</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                    <?php
-                    foreach($db as $x => $valuex){
-                        echo "<tr>";
-                        foreach($valuex as $y => $valuey){
-                            echo "<td style='white-space:nowarp'>";
-                            echo $valuey;
-                            echo "</td>";
+        </script>
+        <div class="container main-container">
+            <h1 class="main-h1">Páciensek</h1>
+            <hr class="main-hr">
+            <div class="main-table">
+                <table id="table-container" style="max-width: none !important; width: 100%" class="table table-striped table-bordered" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <!--lang-->
+                            <th style='white-space:nowarp'>ID</th>
+                            <th style='white-space:nowarp'>NÉV</th>
+                            <th style='white-space:nowarp'>TAJSZÁM</th>
+                            <th style='white-space:nowarp'>SZÜLETÉSI DÁTUM</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <?php
+                        foreach ($db as $x => $valuex) {
+                            echo "<tr>";
+                            foreach ($valuex as $y => $valuey) {
+                                echo "<td style='white-space:nowarp'>";
+                                echo $valuey;
+                                echo "</td>";
+                            }
+                            echo "</tr>";
                         }
-                        echo "</tr>";
-                    }
-                    ?>
-                </tbody>
-            </table>
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
 
 
 
 
-</body>
+    </body>
 
 
-</html>
+    </html>
